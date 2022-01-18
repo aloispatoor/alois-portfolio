0a7ea4b42051e6fc5d0e2f60da1947dedeba3e31
Alois <palois89@posteo.net>
2022-01-18 13:26:12 +0100

    Finishing the HTML, CSS almost done and some js integrations

---
+++ assets/js/sticky.js
@@ -0,0 +1,14 @@
+// Déclarer les variables globales avant la fonction
+let navBar = document.getElementById("desktopNav");
+// OffsetTop paramètre le nb de pixel entre la navBar et le haut de la page
+let sticky = navBar.offsetTop;
+// Fonction pour appeler la classe .sticky en scrollant jusqu'à toucher la navbar
+function getNavSticky(){
+    if (window.pageYOffset >= sticky) {
+      navBar.classList.add("sticky")
+    } else {
+      navBar.classList.remove("sticky");
+    }
+}
+// Appelle la fonction getNavSticky dès qu'on scrolle
+window.onscroll = function() {getNavSticky()};
